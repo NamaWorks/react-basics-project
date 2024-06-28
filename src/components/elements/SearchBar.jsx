99cd90f53555d134b6a99ea1edb73ac1fb84ee98
NamaWorks <pedronayamartinez@hotmail.com>
2024-06-28 19:50:16 +0200

    update components folder and add dotenv dependency, clean app.jsx and prepare API key in doten

---
+++ src/components/elements/SearchBar.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'
+
+const SearchBar = () => {
+  return (
+  <>
+    <input type="text" placeholder='search City'/>
+    <button>get current location</button>
+  </>
+  )
+}
+
+export default SearchBar
