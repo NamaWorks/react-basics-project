70b575d8ec5b76d4722bdb4867da089894479fbf
NamaWorks <pedronayamartinez@hotmail.com>
2024-07-01 21:16:56 +0200

    add mocks intead of requests

--- src/api/getLocation.js
+++ src/api/getLocation.js
@@ -1,3 +1,5 @@
+import { getCurrentLocationMock, weatherFetchMock } from "./dataMock";
+
 export const getCurrentLocation = () => {
 
 navigator.geolocation.getCurrentPosition(function(position){
@@ -11,23 +13,22 @@ navigator.geolocation.getCurrentPosition(function(position){
 }
 
 export const getCityCoordinates = async (city) => {
-  // const cityInput = await document.querySelector("#city-input")
-  // const cityInputValue = cityInput.value
-  // console.log(cityInputValue)
-
-  const res = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q={${city}},{state code}&appid=${import.meta.env.VITE_API_KEY}`)
-  const resJson = await res.json()
+  //* const res = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q={${city}},{state code}&appid=${import.meta.env.VITE_API_KEY}`)
+  // const resJson = await res.json()
+  const resJson = getCurrentLocationMock
   return resJson
+
 }
 
 export const getForecast = async (lat, lng) => {
-const res = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lng}&appid=${import.meta.env.VITE_API_KEY}`)
+ const res = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lng}&appid=${import.meta.env.VITE_API_KEY}`)
   const resJson = await res.json()
   return resJson
 }
 
 export const getWeather = async (lat, lng) => {
-const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&units=metric&appid=${import.meta.env.VITE_API_KEY}`)
-  const resJson = await res.json()
+//* const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&units=metric&appid=${import.meta.env.VITE_API_KEY}`)
+// const resJson = await res.json()
+  const resJson = weatherFetchMock
   return resJson
 }
